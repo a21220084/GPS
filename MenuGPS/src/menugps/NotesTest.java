7b4963524aaf7222190ee3a7e30b74a07f391f34
JoaoPinho <joaopinho@10.65.139.34>
2015-12-11 15:12:51 +0000

    Testes Unitarios

---
+++ MenuGPS/src/menugps/NotesTest.java
@@ -0,0 +1,40 @@
+//package menugps;
+/*
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class NotesTest {
+    Notes n = new Notes("Titulo", "Descrição");
+  @Test
+  public void evaluatesExpression() {
+    n.setNotas("Nota 1");
+    
+    
+    //Teste 1 - Sucesso
+    
+    assertEquals("Titulo",n.getTitulo());
+    assertEquals("Descrição",n.getDescricao());
+    assertEquals("Nota 1",n.getNotas());
+    
+    
+    //Teste 2 - Deu erro como esperardo
+    
+    assertEquals("",n.getTitulo());
+    assertEquals("Descrição",n.getDescricao());
+    assertEquals("Nota 1",n.getNotas());
+    
+    
+    //Teste 3 - Deu erro como esperardo
+    
+    assertEquals("Titulo",n.getTitulo());
+    assertEquals("",n.getDescricao());
+    assertEquals("Nota 1",n.getNotas());
+    
+    
+    //Teste 4 - Deu erro como esperardo
+    
+    assertEquals("Titulo",n.getTitulo());
+    assertEquals("Descrição",n.getDescricao());
+    assertEquals("",n.getNotas());
+  }
+}*/
